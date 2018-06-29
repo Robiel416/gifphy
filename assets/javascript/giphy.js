f9206fb59cc371201a372cd4e7df4f30dc289f46
Robiel <robielasgedom@gmail.com>
2018-06-29 02:22:16 -0400

    update - unable to load api

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,45 +1,51 @@
 
-$(function() {
-    newButton(preloaded, 'searchButton', '#buttons');
+$(function () {
+    showButtons(topics, 'searchButton', '#buttons');
 })
 
-var topics = ['ball', 'hat','chair','plane','shark','cartoon','water']
+var topics = ['ball', 'hat', 'chair', 'plane', 'shark', 'cartoon', 'water']
 
-function newButton(preloaded, classToAdd, areaToAddTo){
+function showButtons(topics, classToAdd, areaToAddTo) {
     $(areaToAddTo).empty();
-    for(var i=0; i<preloaded.length; i++){
-        var g  = $('<button>');
-        g.addClass(classToAdd);
-        g.attr('data-type', preloaded[i]);
-        g.text(preloaded[i]);
-        $(areaToAddTo).append(g);
+    for (var i = 0; i < topics.length; i++) {
+        var newGif = $('<button>');
+        newGif.addClass(classToAdd);
+        newGif.attr('data-type', topics[i]);
+        newGif.text(topics[i]);
+        $(areaToAddTo).append(newGif);
     }
-}
-
-
-$(document).on('click','#search-input',function(){
+};
+//unable to load my api
+$('#buttons').on('click', 'searchButton', function () {
     var type = $(this).data('type');
-    var queryURL = 'https://api.giphy.com/v1/gifs/search?q=' + type +'&api_key=UrbJp6lpDIYtrprxGed4fAoO7qnRyrp4&limit=10';
-    //API call
-    $.ajax({url:queryURL, method: 'GET'})
-    .done(function(response){
-        for(var i=0; i<response.data.length; i++) {
-            var search = $('<div class="search-item">');
-            var rating = response.data[i].rating;
-            var p = $('<p>').text('Rating: '+ rating);
-            var image = $('<img>');
-            var animated = response.data[i].images.fixed_height.url;
-            var still = response.data[i].images.fixed-height_still.url;
-            image.attr('src', still);
-            image.attr('data-still', still);
-            image.attr('data-animated', animated);
-            image.attr('data-state', 'still');
-            image.addClass(searchImage);
-            search.append(p);
-            serach.append(image);
-            $('#favorites').append(search);
-        }
-    })
-})
+    var queryURL = 'http://api.giphy.com/v1/gifs/search?q=' + type + '&api_key=UrbJp6lpDIYtrprxGed4fAoO7qnRyrp4&limit=10';
 
-$()
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    })
+        .then(function(response){
+
+            var results = response.data;
+
+
+            for(var i=0; i<response.data.lenth;i++){
+                var divSearch = $('<div class="search-item">');
+                var rating = response[i].rating;
+                var p = $('<p>').text('Rating: '+rating);
+                var animated = response[i].images.fixed_height.url;
+                var still = response[i].images.fixed_height_still.url;
+                var image = $('<img>');
+                
+                image.attr('src', still);
+                image.attr('data-still', still);
+                image.attr('data-animated', animated);
+                image.attr('data-state', 'still');
+                image.addClass('searchImage')
+
+                divSearch.append(p);
+                divSearch.append(image);
+                $('#favorites').append(divSearch);
+            }
+        })
+})
